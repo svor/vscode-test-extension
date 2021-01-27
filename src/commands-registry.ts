484db94ffe16e4bccd33f5d6c3edda74fc1f48e6
Roman Nikitenko <rnikiten@redhat.com>
2021-01-27 12:17:53 +0200

    Code refactoring
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

--- src/commands-registry.ts
+++ src/commands-registry.ts
@@ -1,13 +1,59 @@
 import * as vscode from "vscode";
+import {
+  createCopyFile,
+  testUriJoinPath,
+  testVsCodeDiffCommand,
+  testVsCodeOpenCommand,
+} from "./commands";
 import { updateWorkspaceLaunchConfigs } from "./workspace-api";
 
-const UPDATE_LAUNCH_CONFIGS = "test-update-workspace-launch-configs";
+namespace TestCommands {
+  export const VSCODE_OPEN = "test-vscode-open";
+  export const VSCODE_DIFF = "test-vscode-diff";
+  export const UPDATE_LAUNCH_CONFIGS = "test-update-workspace-launch-configs";
+  export const CREATE_COPY_FILE = "create-copy-file-for-diff-testing";
+  export const URI_JOIN_PATH = "test-uri-join-path";
+}
+
+export function registerVsCodeOpenCommand(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    TestCommands.VSCODE_OPEN,
+    testVsCodeOpenCommand
+  );
+  context.subscriptions.push(disposable);
+}
+
+export function registerCreateCopyFileCommand(
+  context: vscode.ExtensionContext
+) {
+  const disposable = vscode.commands.registerCommand(
+    TestCommands.CREATE_COPY_FILE,
+    createCopyFile
+  );
+  context.subscriptions.push(disposable);
+}
+
+export function registerVsCodeDiffCommand(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    TestCommands.VSCODE_DIFF,
+    testVsCodeDiffCommand
+  );
+  context.subscriptions.push(disposable);
+}
+
+export function registerUriJoinPathCommand(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand(
+    TestCommands.URI_JOIN_PATH,
+    testUriJoinPath
+  );
+  context.subscriptions.push(disposable);
+}
 
 export function registerUpdateLaunchConfigsCommand(
   context: vscode.ExtensionContext
 ) {
   const disposable = vscode.commands.registerCommand(
-    UPDATE_LAUNCH_CONFIGS,
+    TestCommands.UPDATE_LAUNCH_CONFIGS,
     updateWorkspaceLaunchConfigs
   );
   context.subscriptions.push(disposable);
---
+++ src/commands.ts
@@ -0,0 +1,118 @@
+import { posix } from "path";
+import * as fs from "fs";
+import * as vscode from "vscode";
+
+const defaultFile = "package.json";
+const copyFile = "package_copy.json";
+
+export function testVsCodeOpenCommand(): void {
+  const folder = getWorkspaceFolder();
+  const fileUri = vscode.Uri.parse(posix.join(folder.uri.path, defaultFile));
+
+  const startPosition = new vscode.Position(4, 2);
+  const endPosition = new vscode.Position(7, 4);
+  const range = new vscode.Range(startPosition, endPosition);
+  const options: vscode.TextDocumentShowOptions = { selection: range };
+
+  vscode.commands.executeCommand("vscode.open", fileUri, options);
+}
+
+export async function createCopyFile() {
+  const folder = getWorkspaceFolder();
+
+  // existed file
+  const existedFileUri = vscode.Uri.parse(
+    posix.join(folder.uri.path, defaultFile)
+  );
+
+  // create a new file
+  const newFileUri = vscode.Uri.parse(posix.join(folder.uri.path, copyFile));
+  const createFileCopyedit = new vscode.WorkspaceEdit();
+  createFileCopyedit.createFile(newFileUri, { overwrite: true });
+  await vscode.workspace.applyEdit(createFileCopyedit);
+
+  // copy content from existed file to the new file
+  const copyFileContentEdit = new vscode.WorkspaceEdit();
+  const existedFileDocument = await vscode.workspace.openTextDocument(
+    existedFileUri
+  );
+  const existedFileContent = existedFileDocument.getText();
+
+  // some range for content replacing if a file already exists (the command was run second or third or ... time)
+  const replaceStartPosition = new vscode.Position(2, 0);
+  const replaceEndPosition = new vscode.Position(1000, 0);
+  const replaceRange = new vscode.Range(
+    replaceStartPosition,
+    replaceEndPosition
+  );
+
+  // replace content
+  copyFileContentEdit.replace(newFileUri, replaceRange, existedFileContent);
+  await vscode.workspace.applyEdit(copyFileContentEdit);
+
+  // now the content of the new file should be the same as for the existed file
+  // but we need to have some change to compare two contents within diff editors
+  const provideChangesEdit = new vscode.WorkspaceEdit();
+  provideChangesEdit.replace(
+    newFileUri,
+    new vscode.Range(new vscode.Position(6, 4), new vscode.Position(6, 28)),
+    '"node": ">=10 <12"'
+  );
+  await vscode.workspace.applyEdit(provideChangesEdit);
+
+  // open copy-file
+  vscode.commands.executeCommand("vscode.open", newFileUri);
+}
+
+export async function testVsCodeDiffCommand() {
+  const folder = getWorkspaceFolder();
+
+  // existed file
+  const leftFileUri = vscode.Uri.parse(
+    posix.join(folder.uri.path, defaultFile)
+  );
+
+  // copy file
+  const copyFilePath = posix.join(folder.uri.path, copyFile);
+  const copyFileUri = vscode.Uri.parse(copyFilePath);
+  if (!fs.existsSync(copyFilePath)) {
+    vscode.window.showWarningMessage(
+      "Please create copy file first using 'Create copy file' command!"
+    );
+    return;
+  }
+
+  // provide some selection for testing
+  const selectionStartPosition = new vscode.Position(4, 2);
+  const selectionEndPosition = new vscode.Position(7, 4);
+  const selectionRange = new vscode.Range(
+    selectionStartPosition,
+    selectionEndPosition
+  );
+  const opts: vscode.TextDocumentShowOptions = { selection: selectionRange };
+
+  vscode.commands.executeCommand(
+    "vscode.diff",
+    leftFileUri,
+    copyFileUri,
+    `${defaultFile}` + " - diff",
+    opts
+  );
+}
+
+export function testUriJoinPath() {
+  const folder = getWorkspaceFolder();
+  const fileUri = vscode.Uri.joinPath(folder.uri, defaultFile);
+  vscode.window.showInformationMessage("The path is: " + fileUri.toString());
+}
+
+function getWorkspaceFolder(): vscode.WorkspaceFolder {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  if (!workspaceFolders || workspaceFolders.length < 1) {
+    vscode.window.showWarningMessage(
+      "Please open Theia project as workspace folder for testing!"
+    );
+    throw new Error("A workspace folder is not open");
+  }
+  return workspaceFolders[0];
+}
